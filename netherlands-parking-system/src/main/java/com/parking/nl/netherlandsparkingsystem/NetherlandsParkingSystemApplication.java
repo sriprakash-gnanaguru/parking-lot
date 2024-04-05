adecf08605028a050c93dccaf8525937e7940e96
sriprakash-gnanaguru <sriprakash.gnanaguru@rabobank.nl>
2024-04-05 19:18:47 +0200

    Initial commit

---
+++ netherlands-parking-system/src/main/java/com/parking/nl/netherlandsparkingsystem/NetherlandsParkingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.parking.nl.netherlandsparkingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetherlandsParkingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetherlandsParkingSystemApplication.class, args);
+	}
+
+}
---
+++ netherlands-parking-system/src/test/java/com/parking/nl/netherlandsparkingsystem/NetherlandsParkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.nl.netherlandsparkingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetherlandsParkingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
